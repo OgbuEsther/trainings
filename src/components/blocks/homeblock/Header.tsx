5f31d3d432f0e6e1f0525e856a16f47a8779c3c5
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-05-17 16:15:05 +0100

    header

--- src/components/blocks/homeblock/Header.tsx
+++ src/components/blocks/homeblock/Header.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="w-full h-24 bg-orange-500 flex justify-center items-center">
+      <div className="w-11/12 bg-slate-600 h-full flex ">Header</div>
+    </div>
+  );
 };
 
 export default Header;
